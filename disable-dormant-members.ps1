5851f75d070bf7baa8bb2f14f4474a3f60977a62
xmclej <xmclej@users.noreply.github.com>
2026-06-15 17:43:47 +1200

    Initial commit

---
+++ disable-dormant-members.ps1
@@ -0,0 +1,16 @@
+Connect-MgGraph -NoWelcome -Scopes "User.Read.All","User.ReadWrite.All","AuditLog.Read.All"
+
+$threshold = (Get-Date).AddDays(-45)
+
+Get-MgUser -All -Property “Id,AccountEnabled,SignInActivity”
+| Where-Object {
+    $_.AccountEnabled -eq $true -and
+    $_.UserType -eq "Member" -and (
+        -not $_.SignInActivity.LastSignInDateTime -or
+        [DateTime]$_.SignInActivity.LastSignInDateTime -lt $threshold
+    )
+}
+| Select-Object -First 50
+| ForEach-Object {
+Update-MgUser -UserId $_.Id -AccountEnabled:$false
+}
---
+++ dormant-guests.ps1
@@ -0,0 +1,42 @@
+# Ensure the following module is installed otherwise you will get an error
+# Install-Module Microsoft.Graph -Scope CurrentUser -Force
+
+# To execute run the following command
+# pwsh ./dormant-guests.ps1
+
+#  Only show the first 50 record
+
+Connect-MgGraph -NoWelcome -Scopes "User.Read.All","Group.ReadWrite.All"
+
+$threshold = (Get-Date).AddDays(-45)
+
+Get-MgUser -All -Property Id,AccountEnabled,SignInActivity,DisplayName,UserPrincipalName,UserType `
+| Where-Object {
+    $_.AccountEnabled -eq $true -and
+    $_.UserType -eq "Guest" -and (
+        -not $_.SignInActivity.LastSignInDateTime -or
+        [DateTime]$_.SignInActivity.LastSignInDateTime -lt $threshold
+    )
+}
+| Select-Object DisplayName,
+                UserPrincipalName,
+                @{
+                    Name='UserType'
+                    Expression = { $_.UserType }
+                },
+                @{
+                    Name='LastSignIn'
+                    Expression = { $_.SignInActivity.LastSignInDateTime }
+                },
+                @{
+                    Name='SignInStatus'
+                    Expression = {
+                        if ($_.SignInActivity.LastSignInDateTime) {
+                            "Inactive"
+                        } else {
+                            "Never Signed In"
+                        }
+                    }
+                }
+| Select-Object -First 50
+| Export-Csv -Path "./dormant-guests.csv" -NoTypeInformation -Encoding UTF8
