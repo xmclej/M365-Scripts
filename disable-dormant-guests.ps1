46be0b917cee4402fe64d46c587cc2c648104309
TobyKan1 <tobykan20033@gmail.com>
2026-06-26 14:01:13 +1200

    more tweaks

--- disable-dormant-guests.ps1
+++ disable-dormant-guests.ps1
@@ -2,29 +2,27 @@ Connect-MgGraph -NoWelcome -Scopes "User.Read.All","User.ReadWrite.All","AuditLo
 
 $threshold = (Get-Date).AddDays(-45)
 
+# Exclusion list for guests
 $exclude = @(
     "agoodwin_rcp.co.nz#EXT#@sitesafenz.onmicrosoft.com"
 )
 
-# Step 1: Get all dormant guests
+# Step 1: Get all dormant guests WITH exclusions applied here
 $dormantGuests = Get-MgUser -All -Property Id,AccountEnabled,SignInActivity,DisplayName,UserPrincipalName,UserType `
 | Where-Object {
     $_.AccountEnabled -eq $true -and
-    $_.UserType -eq "Guest" -and (
+    $_.UserType -eq "Guest" -and
+    ($exclude -notcontains $_.UserPrincipalName) -and
+    (
         -not $_.SignInActivity.LastSignInDateTime -or
         [DateTime]$_.SignInActivity.LastSignInDateTime -lt $threshold
     )
 }
 
-# Step 2: Take the first 50 BEFORE exclusions
-$first50 = $dormantGuests | Select-Object -First 50
+# Step 2: Take the first 50 AFTER exclusions
+$finalList = $dormantGuests | Select-Object -First 50
 
-# Step 3: Apply exclusions
-$finalList = $first50 | Where-Object {
-    $exclude -notcontains $_.UserPrincipalName
-}
-
-# Step 4: Export
+# Step 3: Export
 $finalList | Select-Object Id,
                            DisplayName,
                            UserPrincipalName,
@@ -43,10 +41,9 @@ $finalList | Select-Object Id,
                                    }
                                }
                            } `
-| Export-Csv -Path "./disable-dormant-guests.csv" -NoTypeInformation -Encoding UTF8 `
+| Export-Csv -Path "./disable-dormant-guests.csv" -NoTypeInformation -Encoding UTF8
 
-# Step 5: Disable accounts
+# Step 4: Disable accounts (uncomment when ready)
 # $finalList | ForEach-Object {
 #     Update-MgUser -UserId $_.Id -AccountEnabled:$false
 # }
-
--- disable-dormant-members.ps1
+++ disable-dormant-members.ps1
@@ -2,7 +2,7 @@ Connect-MgGraph -NoWelcome -Scopes "User.Read.All","User.ReadWrite.All","AuditLo
 
 $threshold = (Get-Date).AddDays(-45)
 
-# Add any member UPNs you want to exclude
+# Exclusion list
 $exclude = @(
     "accountspayable@sitesafe.org.nz",
     "BCTEST01@sitesafe.org.nz",
@@ -18,25 +18,22 @@ $exclude = @(
     "Ignatuis_precisionconstruction.co.nz#EXT#@sitesafenz.onmicrosoft.com"
 )
 
-# Step 1: Get all dormant members
+# Step 1: Get all dormant members WITH exclusions applied here
 $dormantMembers = Get-MgUser -All -Property Id,AccountEnabled,SignInActivity,DisplayName,UserPrincipalName,UserType `
 | Where-Object {
     $_.AccountEnabled -eq $true -and
-    $_.UserType -eq "Member" -and (
+    $_.UserType -eq "Member" -and
+    ($exclude -notcontains $_.UserPrincipalName) -and
+    (
         -not $_.SignInActivity.LastSignInDateTime -or
         [DateTime]$_.SignInActivity.LastSignInDateTime -lt $threshold
     )
 }
 
-# Step 2: Take the first 50 BEFORE exclusions
-$first50 = $dormantMembers | Select-Object -First 50
+# Step 2: Take the first 50 AFTER exclusions
+$finalList = $dormantMembers | Select-Object -First 50
 
-# Step 3: Apply exclusions
-$finalList = $first50 | Where-Object {
-    $exclude -notcontains $_.UserPrincipalName
-}
-
-# Step 4: Export
+# Step 3: Export
 $finalList | Select-Object Id,
                            DisplayName,
                            UserPrincipalName,
@@ -55,9 +52,9 @@ $finalList | Select-Object Id,
                                    }
                                }
                            } `
-| Export-Csv -Path "./disable-dormant-members.csv" -NoTypeInformation -Encoding UTF8 `
+| Export-Csv -Path "./disable-dormant-members.csv" -NoTypeInformation -Encoding UTF8
 
-# Step 5: Disable 
+# Step 4: Disable accounts (uncomment when ready)
 # $finalList | ForEach-Object {
 #     Update-MgUser -UserId $_.Id -AccountEnabled:$false
 # }
